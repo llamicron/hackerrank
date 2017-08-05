3fbaa0f1c029e1fa471932b06938fbaa4e637577
Luke Sweeney <luke@thesweeneys.org>
2017-08-05 01:13:14 -0500

    Adds solution for plus_minus challenge

---
+++ plus_minus/plus_minus.py
@@ -0,0 +1,28 @@
+#!/bin/python3
+pos = 0
+neg = 0
+zeroes = 0
+
+# Size of the array
+arr_size = int(input())
+arr = [int(x) for x in raw_input().strip().split(" ")]
+# arr_size = 11
+# arr = [1, 2, 3, 4, 5, -6, -7, -8, -9, 0, 0]
+
+for x in arr:
+    if x > 0:
+        pos += 1
+    elif x < 0:
+        neg += 1
+    elif x == 0:
+        zeroes += 1
+
+ratioes = {
+    "pos": float(pos) / float(arr_size),
+    "neg": float(neg) / float(arr_size),
+    "zero": float(zeroes) / float(arr_size)
+}
+
+print round(ratioes['pos'], 4)
+print round(ratioes['neg'], 4)
+print round(ratioes['zero'], 4)
---
+++ plus_minus/readme.md
@@ -0,0 +1,3 @@
+[Challenge](https://www.hackerrank.com/challenges/plus-minus)
+
+This one seems simple, so i'm gonna try it in Python.
